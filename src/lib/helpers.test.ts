aed88c947c41b78363586c4f972af51306703c24
Keith Levi Lumanog <keith@webninjamobile.com>
2023-02-11 01:33:19 +0800

    Update helpers.test.ts

--- src/lib/helpers.test.ts
+++ src/lib/helpers.test.ts
@@ -3,3 +3,6 @@ import { snakeToCapitalize } from './helpers';
 test('this is a sample', () => {
   expect(snakeToCapitalize('snakeCase')).toBe('Snake Case');
 });
+test('this is a sample2', () => {
+  expect(snakeToCapitalize('firstName')).toBe('First Name');
+});
