21519959f42a44b3ad6d80c977885095804ffa5b
keithics <webmaster@keithics.com>
2023-03-13 22:19:39 +0800

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { NotificationsProvider } from '@mantine/notifications';
 import Dashboard from 'components/dashboard/dashboard';
-
 export default function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
